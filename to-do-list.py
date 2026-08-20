a092a799d0fe52d4d31d479af4d5cc5165fbc15b
psyoa14 <psyoa14@nottingham.ac.uk>
2026-08-20 12:32:12 +0100

    using dictionaries

--- to-do-list.py
+++ to-do-list.py
@@ -1,32 +1,30 @@
 class checklist:
     def __init__(self):
-        self.tasks = ["task1", "task2", "task3"]
-        self.status = [False, False, False] #all tasks start as false
+        self.tasks = [{"task_name": "task1", "status": False}, 
+                      {"task_name": "task2", "status": False}, 
+                      {"task_name":"task3", "status": False}]        #all tasks start as false
+                      
+                        
 
-    def add_task(self, name):
-        self.tasks.append(name)
-        self.status.append(False)       #ensure new tasks are also started as false
+    def add_task(self, task_name):
+        self.tasks.append({"task_name": task_name, "status": False})      #ensure new tasks are also started as false
 
     def del_task(self, index):
         self.removed_task = self.tasks.pop(index)
-        self.status.pop(index)
-        print("Deleted task: " + self.removed_task)
+        print("Deleted:", self.removed_task["task_name"])
 
     def toggle_task(self, index):      #ticking and unticking off tasks
-        self.status[index] = not self.status[index]    #task you have selected to mark done, its status will change to the opposite
+        self.tasks[index]["status"] = not self.tasks[index]["status"]    #task you have selected to mark done, its status will change to the opposite
 
 
 my_list = checklist()
 print(my_list.tasks)
 my_list.add_task("task4")
 print(my_list.tasks)
-print(my_list.status)
 my_list.del_task(2)
 print(my_list.tasks)
-print(my_list.status)
 my_list.toggle_task(0)
-print(my_list.status)
-my_list.toggle_task(0)
-print(my_list.status)
+print(my_list.tasks)
+
 
 
